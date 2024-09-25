a13791d3e784037881c7207c4c3f946323a95d98
Andres Ramos <andres.ramos@graniewicz.com>
2024-09-25 02:23:53 +0200

    using statments added for 1D

--- src/ShockwaveIdentifier.jl
+++ src/ShockwaveIdentifier.jl
@@ -1,7 +1,18 @@
 module ShockwaveIdentifier
 
+using Euler2D
+using LaTeXStrings
+using LinearAlgebra
+using Plots
+using Printf
+using ShockwaveProperties
+using Tullio
+using Unitful
+using Dates
+
 include("shock1D.jl")
 include("shock2D.jl")
+
 
 
 end
