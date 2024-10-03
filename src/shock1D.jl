5e2c57a55d2b56e0407035043f5690d2e0057a1f
D.L.C. <71894595+semi-dlc@users.noreply.github.com>
2024-10-03 11:16:34 +0200

    shock1d revised

--- src/shock1D.jl
+++ src/shock1D.jl
@@ -69,79 +69,6 @@ function discontinuities(data, threshold)
     return indices
 end
 
-#= IS THIS FUNCTION NEEDED???? IMPORTANT
-function find_shockwaves(indices::Vector{Int})
-    shockwaves = Vector{Shockwave}()
-    n = length(indices)
-
-    if n == 0
-        return shockwaves
-    end
-
-    xBegin = indices[1]
-
-    for i in 1:(n-1)
-        if indices[i+1] - indices[i] != 1
-            # End of a contiguous sequence
-            push!(shockwaves, Shockwave(xBegin, indices[i]))
-            xBegin = indices[i+1]
-        end
-    end
-
-    # Add the last shockwave if it hasn't been added
-    push!(shockwaves, Shockwave(xBegin, indices[end]))
-
-    return shockwaves
-end
-=#
-
-#=
-	Find all candidates for shockwaves within a 1d Data set.
-
-function findShock1D(data)
-    
-    unit = u"1"
-	try
-		unit = Unitful.unit(data[1])
-	catch 
-		println("Dimensionless data, or corruptes data? ")
-
-	end
-
-	threshold = 0.5 * (averageGradient(data) + maxGradient(data))
-	return discontinuities(data, threshold)
-	#IS THE REST OF THE FUNCTION NEEDED????? IMPORTANT
-	#processing of candidates: filter out the ones
-
-	#probable due to numerical smoothing, the shock wave stretches over multiple cells -> comparison stretched over multiple cells
-	CORRECT_SMOOTHING = 1
-	SAFETY_FACTOR = 0.69
-
-	max_velocity = maximum(broadcast(abs, data))
-	THRESHOLD = max_velocity * SAFETY_FACTOR
-
-	indices = []
-	for i in eachindex(data)[1:end-1-CORRECT_SMOOTHING]
-		if abs(data[i] - data[i+1+CORRECT_SMOOTHING]) >= THRESHOLD
-			push!(indices, i)
-			#push!(indices, i+1)
-			#cprint(" Shockwave detected at ", i)
-		end
-	end
-	#print("\n")
-	
-	for i in eachindex(indices)[1:end-1]
-
-		if indices[i+1] - indices[i] == 1
-			#remove duplicates
-			deleteat!(indices, i)
-		end
-	end
-	
-	return indices 
-end
-=#
-
 function findShock1D(frame, data::EulerSim{1, 3, T}) where{T}
     (t, u_data) = nth_step(data, frame)
 
@@ -151,6 +78,7 @@ function findShock1D(frame, data::EulerSim{1, 3, T}) where{T}
         v = velocity(c)[1]
     end
 
+	#This threshold was chosen to eliminate false positives, but not be overly harsh. It can be changed to the approach for 2D as well.
     threshold = 0.5 * (averageGradient(v_data) + maxGradient(v_data))
 	
 	return discontinuities(v_data, threshold)
