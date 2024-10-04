bd2c1f033eac06733a7c134a38212e20ed92ae26
D.L.C. <71894595+semi-dlc@users.noreply.github.com>
2024-10-04 18:25:47 +0200

    Update demo_full_1d.jl

--- scripts/demo_full_1d.jl
+++ scripts/demo_full_1d.jl
@@ -2,6 +2,13 @@ using ShockwaveIdentifier
 #Example usage for 1D
 
 #Generate png files for each frame 
+#Sod examples
+generate_shock_plots1D("../dataSim/sod_shock_left_1d.tape")
+
+generate_shock_plots1D("../dataSim/sod_shock_right_1d.tape")
+
+
+#Supersonic shock tapes
 generate_shock_plots1D("../dataSim/supersonic_shock_1.tape")
 
 generate_shock_plots1D("../dataSim/supersonic_shock_2.tape")
