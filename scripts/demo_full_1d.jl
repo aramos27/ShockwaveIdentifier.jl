034a1b7b2d53e4238d24a10006f81150ffb959e5
D.L.C. <71894595+semi-dlc@users.noreply.github.com>
2024-10-07 14:32:26 +0200

    Update demo_full_1d.jl

--- scripts/demo_full_1d.jl
+++ scripts/demo_full_1d.jl
@@ -4,16 +4,16 @@ using Base.Threads
 # Example usage for 1D
 example_tapes = [
     # Sod examples
-    #"../dataSim/sod_shock_left_1d.tape",
-    #"../dataSim/sod_shock_right_1d.tape",
+    "../dataSim/sod_shock_left_1d.tape",
+    "../dataSim/sod_shock_right_1d.tape",
     # Supersonic shock tapes
-    #"../dataSim/supersonic_shock_1.tape",
+    "../dataSim/supersonic_shock_1.tape",
     "../dataSim/supersonic_shock_2.tape",
     "../dataSim/supersonic_shock_3.tape"
 ]
 
 # Parallelize the processing of tapes
-@threads for tape in example_tapes
+for tape in example_tapes
     println("Processing $tape on thread $(threadid())")
     generate_shock_plots1D(load_data(tape))
 end
