275d1c120002efe161355456fe580069b8baf2eb
D.L.C. <71894595+semi-dlc@users.noreply.github.com>
2024-10-11 08:06:04 +0200

    Some test sets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,45 @@
 using ShockwaveIdentifier
 using Test
 
+temp_dir = "../tmp"
 @testset "ShockwaveIdentifier.jl" begin
-    # Write your tests here.
+    # (Pre-)Compilation Test
     using ShockwaveIdentifier
+end
+
+@testset "Load tape files" begin
+#array sims
+    #1d tape
+    load_data("../dataSim/supersonic_shock_2.tape")
+    #2d tape
+    load_data("../dataSim/sod_shock_right_2d.tape")
+#cell sims
+    #2d celltape
+    load_data("../dataSim/funky_triangle.celltape")
+end
+
+@testset "Supersonic 1D Test" begin
+    tapes = [
+    "../dataSim/supersonic_shock_1.tape"
+    ]
+
+    for tape in tapes
+        println("Processing $tape")
+        generate_shock_plots1D(load_data(tape), save_dir = temp_dir)
+    end
+
+    rm(temp_dir)
+end
+
+@testset "Sod orb 2D Test" begin
+    tapes = [
+    "../dataSim/sod_shock_orb.tape"
+    ]
+
+    for tape in tapes
+        println("Processing $tape")
+        generate_shock_plots2D(load_data(tape), save_dir = temp_dir)
+    end
+
+    rm(temp_dir)
 end
