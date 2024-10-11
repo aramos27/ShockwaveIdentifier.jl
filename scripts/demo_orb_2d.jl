a605a4f2b9addf2b15284841312c7da22b2034b1
D.L.C. <71894595+semi-dlc@users.noreply.github.com>
2024-10-11 18:07:37 +0200

    Update demo_orb_2d.jl
    Update demo_orb_2d.jl

--- scripts/demo_orb_2d.jl
+++ scripts/demo_orb_2d.jl
@@ -1,9 +1,9 @@
 using ShockwaveIdentifier
 
 #Tape file location
-tape = "dataSim/sod_shock_orb.tape"
+tape = "../dataSim/sod_shock_orb.tape"
 #import tape file as data object
 data = load_data(tape)
 println("Processing $tape ")
 #Find shock waves and plot them directly in the pre-set folder /frames/date-hours-minutes-seconds
-generate_shock_plots2D(data)
+generate_shock_plots2D(data; threshold = 0.2)
