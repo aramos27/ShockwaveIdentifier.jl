232af0754179b1c860170f18fc83dfb6236c08e8
semi-dlc <71894595+semi-dlc@users.noreply.github.com>
2024-10-16 21:25:22 +0200

    Update demo_full_2d.jl

--- scripts/demo_full_2d.jl
+++ scripts/demo_full_2d.jl
@@ -3,11 +3,11 @@ using ShockwaveIdentifier
 # Thresholds exist as preset values,
 # but may need to be tested for each tape file, for which a δ_1_ρ heatmap can be useful.
 tape_data = [
-    "../dataSim/circular_obstacle_radius_1.celltape" 2 0.2;
-    "../dataSim/funky_square.celltape" 2 0.2;
-    "../dataSim/funky_triangle.celltape" 2 0.2; 
-    "../dataSim/sod_shock_right_2d.tape" 1 1.25;
-    "../dataSim/sod_shock_orb.tape" 1 0.11;
+    ("../dataSim/circular_obstacle_radius_1.celltape", 2, 0.2),
+    ("../dataSim/funky_square.celltape", 2, 0.2),
+    ("../dataSim/funky_triangle.celltape", 2, 0.2),
+    ("../dataSim/sod_shock_right_2d.tape", 1, 1.25),
+    ("../dataSim/sod_shock_orb.tape", 1, 0.11),
 ]
 
 for tape in tape_data
