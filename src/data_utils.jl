9970eeb6638f85ec2ff8787c35651dbfb0b56242
Andres Ramos <andres.ramos@graniewicz.com>
2024-10-16 18:52:30 +0200

    All comments for functions added

--- src/data_utils.jl
+++ src/data_utils.jl
@@ -3,6 +3,12 @@ function load_sim_data(filename; T=Float64)
 	return Euler2D.load_euler_sim((filename); T)
 end
 
+"""
+
+    load_data(path; T=Float64)
+
+Load an either array-based or cell-based simulation from path, computed with data type `T`.
+"""
 function load_data(filename; T=Float64)
     if endswith(filename, ".tape")
         return Euler2D.load_euler_sim(filename; T)
@@ -14,11 +20,10 @@ function load_data(filename; T=Float64)
 end
 
 
-#=
-Type support for EulerSim (2D <=> EulerSim{2, 4, T}) and CellBasedEulerSim.
-=#
 
-#Returns matrix with pressure data
+"""
+Returns matrix with pressure data and units for given array-based simulation
+"""
 function compute_pressure_data(frame, data::EulerSim{2,4,T}) where {T}
     (t, u_data) = nth_step(data, frame)
     pressure_data = map(eachslice(u_data; dims=(2,3))) do u
@@ -29,7 +34,9 @@ function compute_pressure_data(frame, data::EulerSim{2,4,T}) where {T}
     return pressure_data
 end
 
-
+"""
+Returns matrix with pressure data and units for given cell-based simulation
+"""
 function compute_pressure_data(frame, data::CellBasedEulerSim)
     p = pressure_field(data, frame, DRY_AIR)
     f = map(p) do val
@@ -38,7 +45,9 @@ function compute_pressure_data(frame, data::CellBasedEulerSim)
     return f
 end
 
-#Returns matrix with velocity data
+"""
+Returns matrix with velocity data and units for given array-based simulation
+"""
 function compute_velocity_data(frame, data::EulerSim{2,4,T}) where {T}
     (t, u_data) = nth_step(data, frame)
     velocity_data = map(eachslice(u_data; dims=(2,3))) do u
@@ -48,7 +57,9 @@ function compute_velocity_data(frame, data::EulerSim{2,4,T}) where {T}
     return velocity_data
 end
 
-
+"""
+Returns matrix with velocity data and units for given cell-based simulation
+"""
 function compute_velocity_data(frame, data::CellBasedEulerSim{T}) where {T}
     v = velocity_field(data, frame)
 
@@ -65,7 +76,9 @@ function compute_velocity_data(frame, data::CellBasedEulerSim{T}) where {T}
     return f
 end
 
-#Returns matrix with density data
+"""
+Returns matrix with density data and units for given array-based simulation
+"""
 function compute_density_data(frame, data::EulerSim{2,4,T}) where {T}
     (t, u_data) = nth_step(data, frame)
     # Compute density data from the conserved properties
@@ -77,6 +90,10 @@ function compute_density_data(frame, data::EulerSim{2,4,T}) where {T}
     return density_data
 end
 
+
+"""
+Returns matrix with density data and units for given cell-based simulation
+"""
 function compute_density_data(frame, data::CellBasedEulerSim)
     rho = density_field(data,frame)
     f = map(rho) do val
@@ -85,7 +102,9 @@ function compute_density_data(frame, data::CellBasedEulerSim)
     return f
 end
 
-#Return matrix of normalized velocity vectors
+"""
+Return matrix of normalized velocity vectors for both cell and array-based simulations
+"""
 function normalized_velocity(frame, data::Union{EulerSim{2,4,T}, CellBasedEulerSim{T}}) where {T}
     velocity_xy = compute_velocity_data(frame, data)
     velocity_xy = ustrip.(velocity_xy)
@@ -102,42 +121,18 @@ function normalized_velocity(frame, data::Union{EulerSim{2,4,T}, CellBasedEulerS
     return velocity_xy
 end
 
+"""
+Return matrix of velocity magnitudes for both cell and array-based simulations
+"""
 function compute_velocity_magnitude_data(frame, data::Union{EulerSim{2,4,T}, CellBasedEulerSim{T}}) where {T}
     v = compute_velocity_data(frame, data)
     v_n = broadcast(norm, v)
     return v_n
 end
 
-
-#Given a matrix A with elements of type [x,y] and a matrix B of type float, it returns A/B
-function divide_matrices_1(matrix1, matrix2)
- 
-    if size(matrix1) != size(matrix2)
-        throw(ArgumentError("Matrices must have the same dimensions"))
-    end
-
-    # Create a new matrix with the same dimensions
-    result = similar(matrix1, eltype(matrix1))
-
-    # Iterate through the matrices and perform the division
-    for i in eachindex(matrix1)
-        for j in eachindex(matrix1, 2)
-            # Extract the values from matrix2 (divisor) and matrix1 (dividend)
-            (x1, y1) = matrix1[i, j]
-            q2 = matrix2[i, j]
-
-            # Divide the elements by the value of the quantity (convert to Float64)
-            x2 = x1 / Float64(q2)
-            y2 = y1 / Float64(q2)
-
-            # Store the result as a tuple in the new matrix
-            result[i, j] = SVector{x2, y2}()
-        end
-    end
-
-    return result
-end
-
+"""
+Given a matrix A with elements of type [x,y] and a matrix B of type float, it returns A/B
+"""
 function divide_matrices(matrix1, matrix2) 
     if size(matrix1) != size(matrix2)
         throw(ArgumentError("Matrices must have the same dimensions"))
