e03aa8c6517a0f3f8cb700e4193171eea380c1ee
Andres Ramos <andres.ramos@graniewicz.com>
2024-10-17 22:27:41 +0200

    Format correction

--- scripts/demo_celltape.jl
+++ scripts/demo_celltape.jl
@@ -1,13 +1,12 @@
 using ShockwaveIdentifier
 
-tapes = [
-    "../dataSim/funky_triangle.celltape"
-    "../dataSim/funky_square.celltape"
-    "../dataSim/circular_obstacle_radius_1.celltape"
+tape_data = [
+    ("../dataSim/circular_obstacle_radius_1.celltape", 2, 0.2),
+    ("../dataSim/funky_square.celltape", 2, 0.2),
+    ("../dataSim/funky_triangle.celltape", 2, 0.2),
+]
 
-    ]
-
-for tape in tapes
+for tape in tape_data
     println("Processing $tape")
-    generate_shock_plots2D(load_data(tape), threshold = 0.2, level=2)
+    generate_shock_plots2D(load_data(tape[1]), vectors=false, level=tape[2], threshold=tape[3])
 end
