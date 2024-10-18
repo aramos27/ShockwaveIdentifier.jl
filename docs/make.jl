9a47a2578362226286d5877f55945e95165dcde0
Andres Ramos <andres.ramos@graniewicz.com>
2024-10-18 02:24:35 +0200

    more documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,16 @@
-using Documenter, ShockwaveIdentifier
+# docs/make.jl
+using Documenter
+using ShockwaveIdentifier
 
-makedocs(sitename="My Documentation")
+makedocs(
+    sitename = "ShockwaveIdentifier.jl Documentation",  # Name of your documentation
+    modules = [ShockwaveIdentifier],                    # The module to document
+    format = Documenter.HTML(),                         # Output format (HTML)
+    pages = [
+        "Home" => "index.md",                           # Main documentation page
+    ]
+)
+
+deploydocs(
+    repo = "github.com/aramos27/ShockwaveIdentifier.jl.git",  # GitHub repository URL
+)
