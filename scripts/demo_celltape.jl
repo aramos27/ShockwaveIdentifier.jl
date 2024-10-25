611d91f3f3d45057e061084d90ef4a55df114bee
D.L.C. <71894595+semi-dlc@users.noreply.github.com>
2024-10-25 23:12:24 +0200

    Update demo_celltape.jl
    Update demo_celltape.jl

--- scripts/demo_celltape.jl
+++ scripts/demo_celltape.jl
@@ -1,12 +1,13 @@
 using ShockwaveIdentifier
 
 tape_data = [
-    ("../dataSim/circular_obstacle_radius_1.celltape", 2, 0.2),
-    ("../dataSim/funky_square.celltape", 2, 0.2),
+    
+    ("../dataSim/funky_square.celltape", 3, 0.2),
+    ("../dataSim/circular_obstacle_radius_1.celltape", 3, 0.2),
     ("../dataSim/funky_triangle.celltape", 2, 0.2),
 ]
 
 for tape in tape_data
     println("Processing $tape")
-    generate_shock_plots2D(load_data(tape[1]), vectors=false, level=tape[2], threshold=tape[3])
+    generate_shock_plots2D(load_data(tape[1]), vectors=false, level=tape[2], threshold=tape[3], debug = true)
 end
